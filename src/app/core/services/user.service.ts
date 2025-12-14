953478d0b7e0f6c6e548744010937c45a184c7ea
sharon20-lgtm <sharonmunozcorales@gmail.com>
2025-12-13 21:29:41 -0500

    refactor(user): usar Partial<AppUser> en createUser para tipado

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
+import { AppUser } from '../../interfaces/app-user.interface';
 
 
 @Injectable({
@@ -9,7 +10,7 @@ export class UserService {
 
   firestore = inject(Firestore);
 
-  createUser(userData: any) {   
+  createUser(userData: Partial<AppUser>) {   
     const ref = doc(this.firestore, `users/${userData.uid}`);
     return setDoc(ref, userData);
   }
